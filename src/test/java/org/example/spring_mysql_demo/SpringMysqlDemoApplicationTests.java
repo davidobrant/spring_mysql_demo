dd3bdc07aa4f945f5211c3e19af305d2c6a6f65d
David Obrant <david.obrant@gmail.com>
2023-12-19 09:17:26 +0100

    initial

---
+++ src/test/java/org/example/spring_mysql_demo/SpringMysqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_mysql_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMysqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
